7459df7ddf6796a57002ee51762c19ae61e1c66e
keidy1402 <fgvkeidy@gmail.com>
2026-05-05 14:55:17 -0300

    Update código2.py

--- código2.py
+++ código2.py
@@ -3,3 +3,22 @@ import pandas as pd
 
 df = pd.read_csv("deputados_2022.csv")
 st. dataframe(df)
+
+#Título do app
+st.title("Consulta de Candidatos por Partido")
+
+#Carregar banco de dados CSV
+df = pd.read_csv("candidatos.csv")
+
+#Entrada do usuário
+sigla = st.text_input("Digite a sigla do partido:")
+
+#Filtragem
+if sigla:
+    resultado = df[df["PARTIDO"].str.upper() == sigla.upper()]
+
+    if not resultado.empty:
+        st.subheader(f"Candidatos do partido {sigla.upper()}:")
+        st.dataframe(resultado)
+    else:
+        st.warning("Nenhum candidato encontrado para essa sigla.")
